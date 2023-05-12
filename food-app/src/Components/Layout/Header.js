d65aaf0f47022fbd3f6ba8d3944527ea6ff4ee2f
niranjantm <niranjantm35@gmail.com>
2023-05-12 20:15:33 +0530

    cart_member

---
+++ food-app/src/Components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React,{Fragment} from "react";
+import foodImage from "../../Assests/foodImg.png"
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+// import BTclasses from "../UI/Button.module.css"
+
+export default function Header(){
+    return(
+        <Fragment>
+            <header className= {classes.header}>
+            <h1>Good Meals</h1>
+           <HeaderCartButton></HeaderCartButton>
+            </header>
+           
+            <div className= {classes["main-image"]}>
+                <img src = {foodImage} alt="Food picture"/>
+            </div>
+        </Fragment>
+    )
+}
