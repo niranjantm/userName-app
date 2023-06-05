1b4dca7c92c13bb47fee893c0b5d0dff6486ed57
niranjantm <niranjantm35@gmail.com>
2023-06-05 22:00:18 +0530

    mon

--- flashcard-app/src/pages/FlashcardDetail.js
+++ flashcard-app/src/pages/FlashcardDetail.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useSelector,useDispatch } from 'react-redux'
-import {useState} from "react"
+import {useState,useRef} from "react"
 function FlashcardDetail() {
 
 const decks =useSelector(state=>state.deck)
 const index = useSelector(state=>state.display)
 const [termIndex,setTermIndex] = useState(0);
+const sliderRef = useRef()
 console.log("index:----",index)
+let width = sliderRef.current.clientWidth;
   return (
-    <div>
-    <div className='bg-yellow-400'>
+    <div className='bg-red-200  h-[200%]'>
+    <div className=''>
        <p className='text-xl'>Flashcard Detail</p>
       <p className='text-2xl m-5 font-bold' >
         {decks[index.index].deckName}
@@ -18,35 +20,65 @@ console.log("index:----",index)
         {decks[index.index].deckDes}
       </p>
     </div>
-    <div className='flex flex-col items-center' >
-     
-      <div className='grid grid-cols-3 w-4/5  bg-red-400  '>
-        <div className='bg-white flex flex-col items-center w-1/4 '>
+ 
+     <div className='flex justify-center'>
+      <div className='flex flex-row w-[70%] px-1 py-1 h-[100%] bg-blue-900 relative '>
+
+        <div className=' relative bg-white flex flex-col overflow-clip items-center min-w-[15%]  '>
           <p>Flashcards</p>
-          <hr className='border border-black w-20 '></hr>
+          <hr className='border border-black w- '></hr>
           {decks[index.index].terms.map((item,index)=>{
             return(
-              <div>
-                <button type="button" className='' onClick={()=>setTermIndex(index)}>{item.term}</button>
+              <div className=''>
+                <button type="button" className='' onClick={()=>{sliderRef.current.scrollLeft = width*index;setTermIndex(index)}}>{item.term}</button>
               </div>
-             
             )
           })}
-          
-         
-          
         </div>
-        <div className='bg-white w-full grid grid-cols-2 gap-2'>
-            <img src={decks[index.index].terms[termIndex].image}></img>
-            <p>{decks[index.index].terms[termIndex].definition}</p>
-            <button>right</button>
-            <button>left</button>
+        <div className='flex flex-row items-start  w-[70%]   h-[100%] px-1 py-1  bg-red-900 relative left-[1%]'>
+          <div className='flex flex-row w-[100%] overflow-hidden bg-blue-200 px-1 py-1 h-96 scroll-smooth'ref={sliderRef} >
+            
+          {decks[index.index].terms.map((item,index)=>{
+            return(
+                <div className=' flex flex-row items-start bg-amber-300  min-w-[100%] mr-1  px-1 py-1' >
+                  <div className='w-[35%] h-[99%] flex mx-2 mr-10 bg-red-500 px-1 py-1  '>
+                  <img className='w-[100%]' src={item.image}></img>
+                 
+                  </div>
+                  <div className='w-[65%] bg-blue-700'>
+                  <p >{item.definition}</p>
+                  </div>
+                  
+                 
+                </div>
+                
+              
+            )
+          })}
           </div>
+        </div>
          
       </div>
-     
+      
+      </div>
+      <div className=' flex justify-center gap-5 min-w-[100%] text-2xl'>
+      <button className="" onClick={()=>{
+        console.log(sliderRef.current.clientWidth)
+       
+        sliderRef.current.scrollLeft -=width
+        if(termIndex)
+        console.log(sliderRef.current.scrollLeft)
+      }}>&lt;</button>
+      <span>{termIndex+1}</span>
+      <button onClick={()=>{
+        console.log(sliderRef.current.clientWidth)
+        let width = sliderRef.current.clientWidth;
+        sliderRef.current.scrollLeft +=width
+        
+      }}>&gt;</button>
+      </div>
     </div>
-    </div>
+
   )
 }
 
